5defb8fa4c3ce482baed678c0a00ee451518329f
Carol42 <caroline.heliose@gmail.com>
2023-01-22 14:53:12 -0300

    :rocket: Criando o loading

--- mobile/mobile/App.tsx
+++ mobile/mobile/App.tsx
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter';
+
+import { Loading } from './src/components/Loading';
+
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </View>
   );
 }
