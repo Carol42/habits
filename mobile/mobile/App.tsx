9a6cdbf7ec587b7d225181871620ab9e081de3c9
Carol42 <caroline.heliose@gmail.com>
2023-01-22 19:53:08 -0300

    :rocket: Aula 02 - criando a Home Screen

--- mobile/mobile/App.tsx
+++ mobile/mobile/App.tsx
@@ -1,3 +1,5 @@
+import './src/lib/dayjs';
+import { Home } from './src/screens/Home';
 import { StatusBar } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import { 
@@ -27,21 +29,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+    <>
+    <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-
-  },
-});
