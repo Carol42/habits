f260276e0f617c218d5a0d71b2e84c88b5ab405f
Carol42 <caroline.heliose@gmail.com>
2023-01-22 23:51:14 -0300

    :rocket: Aula 05 - finalizando o app mobile

--- mobile/mobile/src/components/Checkbox.tsx
+++ mobile/mobile/src/components/Checkbox.tsx
@@ -2,6 +2,8 @@ import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-nativ
 import { Feather } from '@expo/vector-icons'
 import colors from "tailwindcss/colors";
 
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
+
 interface Props extends TouchableOpacityProps {
   title: string;
   checked?: boolean;
@@ -14,16 +16,20 @@ export function Checkbox({ title, checked = false, ...rest }: Props) {
       className="flex-row mb-2 items-center"
       {...rest}
     >
-      { checked 
+      {checked
         ?
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
-          <Feather 
+        <Animated.View
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
+          <Feather
             name="check"
             size={20}
             color={colors.white}
           />
-        </View>
-        : 
+        </Animated.View>
+        :
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       }
 
--- mobile/mobile/src/screens/Home.tsx
+++ mobile/mobile/src/screens/Home.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Text, View, ScrollView, Alert } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { api } from '../lib/axios';
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 
@@ -38,9 +38,9 @@ export function Home() {
     }
   }
 
-  useEffect(() => {
+  useFocusEffect(useCallback(() => {
     fetchData()
-  }, [])
+  }, []))
 
   if (loading) {
     return (
